acb762a25f8a1a7058209bdd0856759c2321729b
Guillaume Meheut <guillaume.meheutpro@gmail.com>
2022-04-05 16:43:56 +0200

    bit of css + del example

--- wordpress/wp-content/themes/marmishlag/page-register.php
+++ wordpress/wp-content/themes/marmishlag/page-register.php
@@ -3,25 +3,23 @@
 if (is_user_logged_in()) {
     wp_redirect( home_url() );
 };
+wp_head();
 
 ?>
-
+<section class='register_container'>
 <h1>Sign up</h1>
 
 <form action="<?= admin_url('admin-post.php'); ?>" method="post" enctype="multipart/form-data">
-    <div>
-        <label for="exampleInputEmail1">Name</label>
-        <input type="text" id="exampleInputEmail1" name="name">
-    </div>
-    <div>
-        <label for="exampleInputEmail1">Email</label>
-        <input type="email" id="exampleInputEmail1" name="email">
-    </div>
-    <div>
-        <label for="exampleInputPassword1" class="form-label">Mot de passe</label>
-        <input type="password" id="exampleInputPassword1" name="password">
-    </div>
+
+        <input placeholder='Name' type="text" id="exampleInputEmail1" name="name">
+
+
+        <input class="emailInput" placeholder='Email' type="email" id="exampleInputEmail1" name="email">
+
+        <input placeholder='password' type="password" id="exampleInputPassword1" name="password">
+
     <input type="hidden" name="action" value="register">
     <?php wp_nonce_field('form', 'form'); ?>
     <button type="submit" class="btn btn-primary">Submit</button>
 </form>
+</section>
